44cf2a4561db81a5fd6337e63654c3dbc989d0c9
John Alonso <jvalon9455@gmail.com>
2020-10-06 18:31:00 -0400

    added other prompts for user to choose from. now to work on the functions

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-const inquirer = require ("inquirer");
+const inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -7,60 +7,67 @@ var connection = mysql.createConnection({
     user: "root",
     password: "Password14",
     database: "employeeDB",
-  });
+});
 
-  connection.connect(function (err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     init();
     connection.end;
-  });
+});
 
 //   start question prompts
 
 function init() {
     console.log("Employee Manager Initialized");
     inquirer
-      .prompt([
-        {
-          name: "selection",
-          message: "What would you like to do?",
-          type: "list",
-          choices: [
-            "View All Employees",
-            "View All Employees by Department",
-            "View All Employees by Manager",
-            "Add Employee",
-          ],
-        },
-      ])
-      .then(({ selection }) => {
-        console.log(selection);
-        if (selection === "View All Employees") {
-          viewAllEmployees();
-        } else if (selection === "View All Employees by Department") {
-          viewEmployeesByDepartment();
-        } else if (selection === "View All Employees by Manager") {
-          viewEmployeesByManager();
-        } else if (selection === "Add Employee") {
-          addEmployee();
-        } 
-      });
-  }
+        .prompt([
+            {
+                name: "selection",
+                message: "What would you like to do?",
+                type: "list",
+                choices: [
+                    "View All Employees",
+                    "View All Employees by Department",
+                    "View All Employees by Manager",
+                    "Add Employee",
+                ],
+            },
+        ])
+        .then(({ selection }) => {
+            console.log(selection);
+            if (selection === "View All Employees") {
+                viewAllEmployees();
+            } else if (selection === "View All Employees by Department") {
+                viewEmployeesByDepartment();
+            } else if (selection === "View Roles") {
+                viewRoles();
+            } else if (selection === "Add Employee") {
+                addEmployee();
+            } else if (selection === "Add Department") {
+                addDepartments();
+            } else if (selection === "Add Roles") {
+                addRoles();
+            } else if (selection === "Update Employee Role") {
+                updateEmployeeRole();
+            }
+        });
+}
 
 //   create function to view all employees
 
-function viewAllEmployees(){
+function viewAllEmployees() {
     connection.query(
         `SELECT employee.first_name, employee.last_name, role.title, department.department_name, role.salary, employee.manager FROM role
         INNER JOIN employee ON employee.role_id = role.id 
         INNER JOIN department ON role.department_id = department.id`,
-        function(err, res){
-            if(err) throw err;
+        function (err, res) {
+            if (err) throw err;
             console.table(res)
             init();
         }
     );
 }
+
 
 
