d31501b106ed3416e0bd35a20e7cf65d4d8732e2
John Alonso <jvalon9455@gmail.com>
2020-10-05 21:15:57 -0400

    initial setup by calling in mysql/inquirer. created connection

--- employeeDB.sql
+++ employeeDB.sql
@@ -54,6 +54,9 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
+    is_manager BOOLEAN,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
     PRIMARY KEY (id)
 );
 
