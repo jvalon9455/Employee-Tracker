d7f60b5d09e05d91d980668784a8dc1dd23bf393
John Alonso <jvalon9455@gmail.com>
2020-10-05 21:41:14 -0400

    tested iniital prompt in node to see if choices populate in terminal

--- server.js
+++ server.js
@@ -16,5 +16,46 @@ var connection = mysql.createConnection({
     connection.end;
   });
 
-  
+//   start question prompts
+
+function init() {
+    console.log("Employee Manager Initialized");
+    inquirer
+      .prompt([
+        {
+          name: "selection",
+          message: "What would you like to do?",
+          type: "list",
+          choices: [
+            "View All Employees",
+            "View All Employees by Department",
+            "View All Employees by Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+          ],
+        },
+      ])
+      .then(({ selection }) => {
+        console.log(selection);
+        if (selection === "View All Employees") {
+          viewAllEmployees();
+        } else if (selection === "View All Employees by Department") {
+          viewEmployeesByDepartment();
+        } else if (selection === "View All Employees by Manager") {
+          viewEmployeesByManager();
+        } else if (selection === "Add Employee") {
+          addEmployee();
+        } else if (selection === "Remove Employee") {
+          removeEmployee();
+        } else if (selection === "Update Employee Role") {
+          updateEmployeeRole();
+        } else if (selection === "Update Employee Manager") {
+          updateEmployeeManager();
+        }
+      });
+  }
+
+//   
 
