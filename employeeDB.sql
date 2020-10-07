662eae97c5767002f95df7fa0da41780e0a4fb94
John Alonso <jvalon9455@gmail.com>
2020-10-06 22:15:31 -0400

    finally got my title  names to equal the department names

--- employeeDB.sql
+++ employeeDB.sql
@@ -29,18 +29,18 @@ CREATE TABLE role (
      PRIMARY KEY (id)
 ); 
 INSERT INTO role (title, salary, department_id)
-VALUES ("R&D Team Lead", 120000, 4),
-("Lead Engineer", 150000, 7),
-("Software Engineer", 120000, 7),
-("Sales Lead", 95000, 1), 
-("Salesperson", 80000, 1),
-("Marketing Consultant", 80000, 2),
-("Marketing Analyst", 110000, 2),
-("Accountant", 100000, 3),
-("Account Manager", 150000, 3),
-("HR Director", 90000, 5),
-("Lawyer", 200000, 6),
-("Legal Team Lead", 250000, 6);
+VALUES ("R&D Team Lead", 120000, 1),
+("Lead Engineer", 150000, 2),
+("Software Engineer", 120000, 2),
+("Sales Lead", 95000, 3), 
+("Salesperson", 80000, 3),
+("Marketing Consultant", 80000, 4),
+("Marketing Analyst", 110000, 4),
+("Accountant", 100000, 5),
+("Account Manager", 150000, 5),
+("HR Director", 90000, 6),
+("Lawyer", 200000, 7),
+("Legal Team Lead", 250000, 7);
 
 
 CREATE TABLE employee (
@@ -56,16 +56,16 @@ CREATE TABLE employee (
 
 INSERT INTO employee (first_name, last_name, role_id, manager)
 VALUES 
-("Michael", "Scott", 2, "Jan Levinson"),
+("Michael", "Scott", 4, "Jan Levinson"),
 ("Jim", "Halpert", 7, "Asian Jim"),
-("Dwight", "Schrute", 8, "Ryan Howard"),
-("Pamela", "Beasley", 7, null), 
-("Stanley", "Hudson", 5, "Malia Brown"),
-("Angela", "Martin", 4, "Holly Flax"),
-("Andy", "Bernard", 8, "Kelly Kapoor"),
-("Meredith", "Palmer", 12, "Karen Filippelli"),
-("Kevin", "Malone", 11, null),
-("Oscar", "Nunez", 7, null);
+("Dwight", "Schrute", 1, "Ryan Howard"),
+("Pamela", "Beasley", 1, null), 
+("Stanley", "Hudson", 2, "Malia Brown"),
+("Angela", "Martin", 2, "Holly Flax"),
+("Andy", "Bernard", 3, "Kelly Kapoor"),
+("Meredith", "Palmer", 5, "Karen Filippelli"),
+("Kevin", "Malone", 6, null),
+("Oscar", "Nunez", 6, null);
 
 
 
